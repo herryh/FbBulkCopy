ad147c3f1080fdff7fa5c8728c988905ad952467
Herry Hamidjaja <herryh@unimelb.edu.au>
2018-10-02 10:10:13 +1000

    Initial Version

---
+++ FbExample/Program.cs
@@ -0,0 +1,76 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using FirebirdSql.Data.FirebirdClient.Extensions;
+using System.IO;
+using System.Text;
+
+namespace FbExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbPath = "bulktest.fdb";
+           
+            var connectionString = new FbConnectionStringBuilder
+            {
+                Database = dbPath,
+                ServerType = FbServerType.Embedded,
+                UserID = "SYSDBA",
+                Password = "masterkey",
+                ClientLibrary = "fbclient.dll",
+                Dialect = 3
+            }.ToString();
+
+            if (!File.Exists(dbPath))
+            {
+                InitSourceDatabase(connectionString);
+
+            }
+
+            using (var conn = new FbConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+
+                // Create destination table
+                cmd.CommandText = "CREATE TABLE D(A int, B varchar(20), C BLOB sub_type 0);";
+                cmd.ExecuteNonQuery();
+
+                // Query source table
+                cmd.CommandText = "SELECT A,B,C FROM S";
+                var reader = cmd.ExecuteReader();
+
+                var bulkcopy = new FbBulkCopy(conn)
+                {
+                    DestinationTableName = "D"
+                };
+                bulkcopy.WriteToServer(reader);
+            }
+        }
+
+        private static void InitSourceDatabase(string connectionString)
+        {
+            FbConnection.CreateDatabase(connectionString);
+            using (var conn = new FbConnection(connectionString))
+            {
+                conn.Open();
+                // create table source
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "CREATE TABLE S(A int, B varchar(20), C BLOB sub_type 0);";
+                cmd.ExecuteNonQuery();
+
+                var sb = new StringBuilder();
+                sb.AppendLine("execute block\r\n as\r\n begin");
+                for (var i = 0; i < 5; i++)
+                {
+                    sb.Append($"insert into S(A,B,C) values('{i}','{i}',x'504B03041400000008009484C74CBA2A2857CB010000F70300002E00240055736572732F68656E6C6579732F417070446174612F4C6F63616C2F54656D702F322F746D70413030422E746D700A0020000000000001001800678B4DE529FED301678B4DE529FED301678B4DE529FED3017D53C18ADB3010FD9581B2D01ED6D8EE66374DDA82490F5B2865A13DEA22CB6347204B6624278425FFDE91ED24DE64A90FD6E8CD9BA79927F42A28D49990D6EB57513B1B4269384845EDF7DA7B28484BB33E329489BA751592858DEB4923C16FDCAF8F9C52CE38E23A4158A5A221449B8AD2F498AE6FA07CB19883BCBD659EC01337EE6F54AFC01337CB97231C830B3BFB924F300711E6F57D3EAFB75D5F816FB5D3F705CECAF3AEAFC013F73CCC5B3F6EE038E571BC9D6C293A49C0F7024300CF25149982FF7C42D50FF0983CC13347E988DDCDF21F011E9214EE99C3D94FF0613CE1FC831783D223581710086B24B48A23691BF420BBCE1C203828AADE043F566C5CDBA20D1E9C855FB2849FD50AB2CF8BFC7199E5D7EA458BA495B4F043CB12034B16DE3BA565D05C4DA806AD8A4F8220A9C100AE86AF4F497AC7B3C7D1A5AD26295DC3F76FCB4B86F01E77D2F4A31497852D92EC0EC9948F393EAF958738DC242295EA4906E4A97858832AC43268915B6C4CAF1C5B61708706B4858EA58741F73A6CC1BB16E3858C423BC98F683081C06FF9A9A0C58A352B50680CF89E769A3B48665E6C8AD9E62F7A2E662B3BC3B6B08343D3F3DC0BFBA26D03B09A4AFEC8483EE3AB8BCF4370FC07504B01022D001400000008009484C74CBA2A2857CB010000F70300002E002400000000000000200000000000000055736572732F68656E6C6579732F417070446174612F4C6F63616C2F54656D702F322F746D70413030422E746D700A0020000000000001001800678B4DE529FED301678B4DE529FED301678B4DE529FED301504B05060000000001000100800000003B0200000000');");
+                    
+                }
+                sb.Append("\r\n");
+                sb.Append("end");
+                cmd.CommandText = sb.ToString();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ FbExtension/FbBulkCopy.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebirdSql.Data.FirebirdClient.Extensions
+{
+    /// <summary>
+    /// Lets you efficiently bulk load a Firebird table with data from another source.
+    /// It uses table-less select * from rdb$database with union all
+    /// 
+    /// Author: Herry Hamidjaja
+    /// Date: 12/08/2018 
+    /// </summary>
+    public class FbBulkCopy : IDisposable
+    {
+        #region "Field(s)"
+        private bool _defaultCloseAndDisploseDataReader = true;
+        private int _defaultBatchSize = 1000;
+        private int _defaultCopyTimeout = 60; // seconds
+        private FbConnection _cn;
+        private string _tableNm;
+        private Dictionary<string, FbColumnTypesExtended> _columns = null;
+        public Dictionary<string, FbColumnTypesExtended> ColumnMappings
+        {
+            get
+            {
+                return _columns;
+            }
+        }
+
+        #endregion
+
+        #region "Properties"
+        /// <summary>
+        /// Name of the destination table in the database.
+        /// </summary>
+        public string DestinationTableName
+        {
+            get
+            {
+                return _tableNm;
+            }
+            set
+            {
+                _tableNm = value;
+                if (!string.IsNullOrEmpty(value))
+                    SetColumnMetaData();
+            }
+        }
+        /// <summary>
+        /// Allows control of the incoming DataReader by closing and disposing of it by default after all bulk copy 
+        /// operations have completed if set to TRUE, if set to FALSE you need to do your own cleanup (this is useful 
+        /// when your DataReader returns more than one result set).  By default this is set to TRUE.
+        /// </summary>
+        public bool CloseAndDisploseDataReader { get; set; }
+        /// <summary>
+        /// Batch size data is chunked into when inserting.  The default size is 1,000 records per batch.
+        /// </summary>
+        public int BatchSize { get; set; }
+        /// <summary>
+        /// The time in seconds to wait for a batch to load. The default is 30 seconds.
+        /// </summary>
+        public int BulkCopyTimeout { get; set; }
+
+        #endregion
+
+        #region "Constructors"
+        /// <summary>
+        /// Initializes a new instance of the SqliteBulkCopy class using the specified open instance of SqliteConnection.
+        /// </summary>
+        /// <param name="connection"></param>
+        public FbBulkCopy(FbConnection connection)
+        {
+            CloseAndDisploseDataReader = _defaultCloseAndDisploseDataReader;
+            this.BatchSize = _defaultBatchSize;
+            this.BulkCopyTimeout = _defaultCopyTimeout;
+            _cn = connection;
+            if (_cn.State == ConnectionState.Closed)
+                _cn.Open();
+        }
+
+        /// <summary>
+        /// Initializes and opens a new instance of SqliteBulkCopy based on the supplied connectionString. 
+        /// The constructor uses the SqliteConnection to initialize a new instance of the SqliteBulkCopy class.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public FbBulkCopy(string connectionString)
+        {
+            this.CloseAndDisploseDataReader = _defaultCloseAndDisploseDataReader;
+            this.BatchSize = _defaultBatchSize;
+            this.BulkCopyTimeout = _defaultCopyTimeout;
+            _cn = new FbConnection(connectionString);
+            if (_cn.State == ConnectionState.Closed)
+                _cn.Open();
+        }
+        #endregion
+
+        #region "Method(s) and Enum(s)"
+        /// <summary>
+        /// Defines the mapping between a column in a SqliteBulkCopy instance's data source and a column in the instance's destination table.
+        /// </summary>
+        public enum FbColumnTypesExtended
+        {
+            //
+            // Summary:
+            //     A signed integer.
+            Integer = 1,
+            //
+            // Summary:
+            //     A floating point value.
+            Real = 2,
+            //
+            // Summary:
+            //     A text string.
+            Text = 3,
+            //
+            // Summary:
+            //     A blob of data.
+            Blob = 4,
+            // Summary:
+            //     A date column
+            Date = 5
+        }
+
+        /// <summary>
+        /// Get column metadata for a table in a Sqlite database
+        /// </summary>
+        private void SetColumnMetaData()
+        {
+            if (!string.IsNullOrEmpty(this.DestinationTableName))
+            {
+                _columns = new Dictionary<string, FbColumnTypesExtended>();
+                /*
+                    7 = SMALLINT
+                    8 = INTEGER
+                    10 = FLOAT
+                    12 = DATE
+                    13 = TIME
+                    14 = CHAR
+                    16 = BIGINT
+                    27 = DOUBLE PRECISION
+                    35 = TIMESTAMP
+                    37 = VARCHAR
+                    261 = BLOB
+                 */
+                var sql = string.Format(@"SELECT R.RDB$FIELD_NAME AS name,
+                            CASE F.RDB$FIELD_TYPE
+                             WHEN 7 THEN 'SMALLINT'
+                             WHEN 8 THEN 'INTEGER'
+                             WHEN 10 THEN 'FLOAT'
+                             WHEN 12 THEN 'DATE'
+                             WHEN 13 THEN 'TIME'     
+                             WHEN 14 THEN 'CHAR'
+                             WHEN 16 THEN 'INT64'
+                             WHEN 27 THEN 'DOUBLE'
+                             WHEN 35 THEN 'TIMESTAMP'
+                             WHEN 37 THEN 'VARCHAR'
+                             WHEN 261 THEN 'BLOB'
+                             ELSE 'UNKNOWN'
+                            END AS type,
+                            F.RDB$FIELD_LENGTH AS field_length,
+                            CSET.RDB$CHARACTER_SET_NAME AS field_charset
+                            FROM RDB$RELATION_FIELDS R
+                            LEFT JOIN RDB$FIELDS F ON R.RDB$FIELD_SOURCE = F.RDB$FIELD_NAME
+                            LEFT JOIN RDB$CHARACTER_SETS CSET ON F.RDB$CHARACTER_SET_ID = CSET.RDB$CHARACTER_SET_ID
+                            WHERE R.RDB$RELATION_NAME= '{0}'
+                            ORDER BY R.RDB$FIELD_POSITION", this.DestinationTableName);
+                using (var cmd = new FbCommand(sql, _cn) { CommandType = CommandType.Text })
+                {
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var key = dr["name"].ToString().Trim();
+                            var typ = new FbColumnTypesExtended();
+                            var columnType = dr["type"].ToString().Trim().ToUpper();
+                            switch (columnType)
+                            {
+                                case "INTEGER":
+                                case "SMALLINT":
+                                case "BIGINT":
+                                    typ = FbColumnTypesExtended.Integer;
+                                    break;
+                                case "DOUBLE":
+                                case "FLOAT":
+                                    typ = FbColumnTypesExtended.Real;
+                                    break;
+                                case "CLOB":
+                                    typ = FbColumnTypesExtended.Text;
+                                    break;
+                                case "BLOB":
+                                    typ = FbColumnTypesExtended.Blob;
+                                    break;
+                                case "DATE":
+                                case "TIMESTAMP":
+                                    typ = FbColumnTypesExtended.Date;
+                                    break;
+                                default: // look for fringe cases that need logic
+                                    if (columnType.StartsWith("CHAR"))
+                                        typ = FbColumnTypesExtended.Text;
+                                    if (columnType.StartsWith("VARCHAR"))
+                                        typ = FbColumnTypesExtended.Text;
+                                    if (columnType.StartsWith("NCHAR"))
+                                        typ = FbColumnTypesExtended.Text;
+                                    break;
+                            }
+                            _columns.Add(key, typ);
+                        }
+                    }
+                }
+                if (_columns == null || _columns.Count < 1)
+                    throw new Exception($"{this.DestinationTableName} could not be found in the database");
+            }
+        }
+
+        /// <summary>
+        /// Close and database connections.
+        /// </summary>
+        public void Close()
+        {
+            if (_cn.State == ConnectionState.Open)
+                _cn.Close();
+        }
+
+        /// <summary>
+        /// Copies all rows in the supplied IDataReader to a destination table specified 
+        /// by the DestinationTableName property of the SqliteBulkCopy object.
+        /// </summary>
+        /// <param name="reader"></param>
+        public void WriteToServer(IDataReader reader)
+        {
+            if (reader == null)
+                throw new ArgumentNullException("reader");
+
+            // build the insert schema and use table-less union all
+            var insertClause = new StringBuilder();
+            insertClause.Append($"INSERT INTO {this.DestinationTableName} (");
+            var first = true;
+            foreach (var c in this.ColumnMappings)
+            {
+                if (first)
+                {
+                    insertClause.Append(c.Key);
+                    first = false;
+                }
+                else
+                    insertClause.Append("," + c.Key);
+            }
+            insertClause.Append(")");
+
+            first = true;
+            var valuesClause = new StringBuilder();
+            var currentBatch = 0;
+            while (reader.Read())
+            {
+                // generate insert values block statement
+                valuesClause.Append("SELECT ");
+                var colFirst = true;
+                foreach (var c in this.ColumnMappings)
+                {
+                    if (!colFirst)
+                        valuesClause.Append(",");
+                    else
+                        colFirst = false;
+                    var columnValue = reader[c.Key] == null ? null as string : reader[c.Key].ToSQLString();
+                    if (string.IsNullOrEmpty(columnValue))
+                        valuesClause.Append("NULL");
+                    else
+                    {
+                        switch (c.Value)
+                        {
+                            case FbColumnTypesExtended.Date:
+                                try
+                                {
+                                    valuesClause.Append($"'{ DateTime.Parse(columnValue).ToString("yyyy-MM-dd HH:mm:ss") }'");
+                                }
+                                catch (Exception exp)
+                                {
+                                    throw new Exception($"Invalid Cast when loading date column [{ c.Key }] in table [{ this.DestinationTableName}] in Sqlite DB with data; value being casted '{ columnValue}', incoming values must be of data format consumable by .NET; error:\n {exp.Message}");
+                                }
+                                break;
+                            case FbColumnTypesExtended.Integer:
+                            case FbColumnTypesExtended.Real:
+                                valuesClause.Append(columnValue);
+                                break;
+                            case FbColumnTypesExtended.Blob:
+                                valuesClause.Append($"X'{columnValue}'");
+                                break;
+                            case FbColumnTypesExtended.Text:
+                            default:
+                                valuesClause.Append($"'{columnValue.Replace("'", "''")}'");
+                                break;
+                        }
+                    }
+                }
+                valuesClause.Append("FROM rdb$database UNION ALL ");
+
+                currentBatch++;
+                // limit the query to under 64kb as the max limit firebird query
+                if (valuesClause.Length + insertClause.Length > 60000 || currentBatch == this.BatchSize)
+                {
+                    valuesClause.Length -= " UNION ALL ".Length;
+                    var dml = $"EXECUTE BLOCK AS BEGIN {insertClause.ToString()} {valuesClause.ToString()}; END";
+                    valuesClause.Clear();
+                    using (var cmd = new FbCommand(dml, _cn) { CommandType = CommandType.Text, CommandTimeout = this.BulkCopyTimeout })
+                        cmd.ExecuteNonQuery();
+                    currentBatch = 0;
+                }
+            }
+            if (this.CloseAndDisploseDataReader)
+            {
+                reader.Close();
+                reader.Dispose();
+            }
+            // if any records remain after the read loop has completed then write them to the DB
+            if (currentBatch > 0)
+            {
+                valuesClause.Length -= " UNION ALL ".Length;
+                var dml = $"EXECUTE BLOCK AS BEGIN {insertClause.ToString()} {valuesClause.ToString()}; END";
+                using (var cmd = new FbCommand(dml, _cn) { CommandType = CommandType.Text, CommandTimeout = this.BulkCopyTimeout })
+                    cmd.ExecuteNonQuery();
+            }
+
+        }
+
+#pragma warning disable 1998
+        /// <summary>
+        /// The asynchronous version of WriteToServer, which copies all rows in the supplied IDataReader to a 
+        /// destination table specified by the DestinationTableName property of the SqliteBulkCopy object.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private async Task WriteToServerAsyncInternal(IDataReader reader)
+        {
+            WriteToServer(reader);
+        }
+#pragma warning restore 1998
+
+        /// <summary>
+        /// Releases all resources used by the current instance of the SqliteBulkCopy class.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Close();
+            _cn.Dispose();
+        }
+
+        #endregion
+    }
+
+    static class FbObjectExtension
+    {
+        /// <summary>
+        /// Provide Extension method to convert object to string and handle array of byte for varbinary.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static string ToSQLString(this object a)
+        {
+            if (a.GetType().Name == "Byte[]")
+            {
+                return BitConverter.ToString(a as byte[]).Replace("-", "");
+            }
+            return a.ToString();
+        }
+
+
+    }
+}
